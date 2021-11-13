9933eb0594a9597a2912ca2dcfb3d5ba3be2bbc8
micahwaweru <micah.waweru@gmail.com>
2021-11-13 01:07:04 -0600

    build

--- index.js
+++ index.js
@@ -1,31 +1,218 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
+const inquirer = require('inquirer')
+const fs=require('fs');
+const path=require('path')
+//const jest = require('jest')
 
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
-inquirer.prompt([
-    {
-        type: 'input',
-        message: 'Employee Name',
-        name: 'employeeName',
-    },
-    {
-        type: 'input',
-        message: 'Employee Email',
-        name: 'employeeEmail',
-    },
-    {
-        type: 'input',
-        message: 'Employee GitHub',
-        name: 'employeeGitHub',
+const DIST_DIR = path.resolve(__dirname, 'dist')
+const outputPath = path.join(DIST_DIR, 'index.html');
+const render = require('./src/pageTemplate');
+
+const teamArray = [];
+const idArray = [];
+
+function init(){
+    function addManager(){
+        console.log('Assemble your crew!');
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "What's manager's name?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter the team's manager's name.";
+                }
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: "What's the manager's ID?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter a valid Manager's ID.";
+                }
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "What's the manager's email?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Email address can't be empty.";
+                }
+            },
+            {
+                type: "input",
+                name: "managerOfficeNumber",
+                message: "What's the manager's office number? (format: 111111111)",
+                validate: answer => {
+    
+                    if (answer) {
+                        return true;
+                    }
+                    return "Phone number cannot be empty.";
+                }
+            }
+        ]).then(answers => {
+            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+            teamArray.push(manager);
+            idArray.push(answers.managerId);
+            addTeam();
+        });
+        
     }
+    function addTeam(){
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "memberChoice",
+                message: "What would you like to add next?",
+                choices: [
+                    "Engineer",
+                    "Intern",
+                    "End application"
+                ]
+            }
+        ]).then(userChoice => {
+            switch (userChoice.memberChoice) {
+                case "Engineer":
+                    addEngineer();
+                    break;
+                case "Intern":
+                    addIntern();
+                    break;
+                default:
+                    generateHTML();
+            }
+        });
+    }
+    function addEngineer() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "engineerName",
+                message: "What's the engineer's name?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Engineer's name can't be left empty.";
+                }
+            },
+            {
+                type: "input",
+                name: "engineerId",
+                message: "What's the engineer's id?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter a valid Engineer's ID.";
+                }
+            },
+            {
+                type: "input",
+                name: "engineerEmail",
+                message: "What's the engineer's email?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Email address can't be empty.";
+                }
+            },
+            {
+                type: "input",
+                name: "engineerGithub",
+                message: "What's the engineer's GitHub username?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter the engineer's GitHub username.";
+                }
+            }
+        ]).then(answers => {
+            const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+            teamArray.push(engineer);
+            idArray.push(answers.engineerId);
+            addTeam();
+        });
+    }
+    function addIntern() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "internName",
+                message: "What's the intern's name?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter at least one character.";
+                }
+            },
+            {
+                type: "input",
+                name: "internId",
+                message: "What's the intern's id?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter a valid Intern's ID.";
+                }
+            },
+            {
+                type: "input",
+                name: "internEmail",
+                message: "What's the intern's email?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Email address can't be empty.";
+                }
+            },
+            {
+                type: "input",
+                name: "internSchool",
+                message: "What's the intern's school?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter a correct school.";
+                }
+            }
 
-])
+        ]).then(answers => {
+            const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
+            teamArray.push(intern);
+            idArray.push(answers.internId);
+            addTeam();
+        });
+    }
+    function generateHTML() {
 
-.then((responses)=>{
-    console.log(responses);
-    var nameString = JSON.stringify(responses.employeeName);
-    var emailString = JSON.stringify(responses.employeeEmail);
-    var githubString = JSON.stringify(responses.employeeGitHub);
-
-    console.log(nameString, emailString, githubString);
-})
+        // Create dist directory for index.html if it doesnt exist
+        if (!fs.existsSync(DIST_DIR)) {
+            fs.mkdirSync(DIST_DIR)
+        }
+        console.log("Generating Team Profile.... ");
+        fs.writeFileSync(outputPath, render(teamArray), "utf-8");
+    }
+    addManager();
+}
+init();
